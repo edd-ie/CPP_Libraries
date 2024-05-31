5c5038a76b7616f04a819c2aace4a4613b5879d6
edd-ie <ogendoeddie@gmail.com>
2024-05-31 02:30:13 -0300

    Sudoku Reader

---
+++ Sudoku.h
@@ -0,0 +1,23 @@
+//
+// Created by _edd.ie_ on 31/05/2024.
+//
+
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+// Printing the Sudoku grid
+
+inline void printPuzzle(int puzzle[][9], int size = 9)
+{
+    std::cout << "-----------------\n";
+    for (int i = 0; i<size; i++) {
+
+        for (const int val : puzzle[i]) {
+            std::cout << val << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "-----------------\n";
+}
+
+#endif //SUDOKU_H
