0e879a2c08bb9df135dbb269b24f38cb61e4d89c
edd-ie <ogendoeddie@gmail.com>
2024-05-31 02:28:52 -0300

    Sudoku Reader

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "LinkedList.h"
 #include "TestFunctions.h"
 #include "Sorting.h"
+#include "Sudoku.h"
 
 
 int main()
@@ -75,6 +76,20 @@ int main()
     insertion(dataSet, 7);
 
     printArray(dataSet,7);
+
+    int puzzle[9][9] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+
+    printPuzzle(puzzle);
 
     return 0;
 }
